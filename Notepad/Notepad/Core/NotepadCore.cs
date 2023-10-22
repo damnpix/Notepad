d6d100c65f42b439b93a8563d5a77a830bd7ee9c
hexsparky <111631956+hexsparky@users.noreply.github.com>
2023-10-22 22:28:19 +0300

    Add files via upload

---
+++ Notepad/Notepad/Core/NotepadCore.cs
@@ -0,0 +1,80 @@
+﻿using Notepad.Windows;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Notepad.Core
+{
+    internal class NotepadCore
+    {
+        private static string _savedFileLocation;
+        private static bool _saved = false;
+
+        public static void NewFileCreate(RichTextBox richTextBox, mainWin mainWin)
+        {
+            if (_saved)
+            {
+                mainWin.Text = "Untitled";
+                richTextBox.Clear();
+                _saved = false;
+            }
+            else richTextBox.Clear();
+        }
+        public static void Exit()
+        {
+            Application.Exit();
+        }
+        public static void Edit_Clear(RichTextBox richTextBox)
+        {
+            richTextBox.Clear();
+        }
+        public static void Edit_Undo(RichTextBox richTextBox)
+        {
+            richTextBox.Undo();
+        }
+        public static void Edit_Redo(RichTextBox richTextBox)
+        {
+            richTextBox.Redo();
+        }
+        public static void Save(RichTextBox richTextBox,SaveFileDialog saveFileDialog,mainWin mainWin)
+        {
+            if (_saved == false)
+            {
+                if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(saveFileDialog.FileName))
+                    {
+                        _savedFileLocation = saveFileDialog.FileName;
+                        streamWriter.Write(richTextBox.Text);
+                    }
+                    _saved = true;
+                    mainWin.Text = Path.GetFileNameWithoutExtension(saveFileDialog.FileName);
+                }
+            }
+            else if(_saved == true) {
+                using (StreamWriter streamWriter = new StreamWriter(_savedFileLocation))
+                {
+                    streamWriter.Write(richTextBox.Text);
+                }
+            }
+        }
+        public static void Open(RichTextBox richTextBox,OpenFileDialog openFileDialog,mainWin mainWin)
+        {
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamReader streamReader = new StreamReader(openFileDialog.FileName))
+                {
+                    mainWin.Text = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                    richTextBox.Text = streamReader.ReadToEnd();
+                }
+            }
+        }
+        public static void Preferences_zoom(RichTextBox richTextBox,int value)
+        {
+            richTextBox.ZoomFactor = value;
+        }
+        public static void About(aboutWin aboutWin)
+        {
+            aboutWin.ShowDialog();
+        }
+    }
+}
---
+++ Notepad/Notepad/Windows/aboutWin.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Notepad.Windows
+{
+    partial class aboutWin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(aboutWin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Notepad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "@hexsparky";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(177, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // aboutWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(279, 115);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(237)))), ((int)(((byte)(243)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "aboutWin";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
